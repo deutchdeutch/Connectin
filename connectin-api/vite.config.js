c218441711f796b2cfb6b19189ce225f9c8800a0
Aurelien Deutchoua <aurelien.deutchoua@epitech.eu>
2026-04-21 17:01:16 +0200

    correction de vite.config.js

--- connectin-api/vite.config.js
+++ connectin-api/vite.config.js
@@ -1,21 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-    plugins: [
-        react(),          
-        tailwindcss(),     
-
-    ],
-    
-    base: "/connectin/",  
-    build: {
-        outDir: "dist",    
-        sourcemap: true,
-    },
-    server: {
-        host: true,        
-        port: 5173
-    }
+  plugins: [react()],
+  base: "/connectin/",
+  root: "resources", 
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+  },
 });
