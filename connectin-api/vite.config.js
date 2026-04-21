3a43ad3f7dffd7341cd1c736e69f64d3afc89b57
Aurelien Deutchoua <aurelien.deutchoua@epitech.eu>
2026-04-21 16:36:55 +0200

    configuration de vite.config.js pour faciliter la mise sur github page

--- connectin-api/vite.config.js
+++ connectin-api/vite.config.js
@@ -1,18 +1,21 @@
 import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: ["resources/css/app.css", "resources/js/app.jsx"],
-            refresh: true,
-        }),
-        tailwindcss(),
-        react(),
+        react(),          
+        tailwindcss(),     
+
     ],
-    server: {
-        host: "127.0.0.1",
+    
+    base: "/connectin/",  
+    build: {
+        outDir: "dist",    
+        sourcemap: true,
     },
+    server: {
+        host: true,        
+        port: 5173
+    }
 });
