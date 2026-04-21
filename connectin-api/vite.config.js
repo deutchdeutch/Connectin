07101a27e567a1d0e26b0fb3e3de1b16d0edda29
Aurelien Deutchoua <aurelien.deutchoua@epitech.eu>
2026-04-21 19:09:20 +0200

    connectin phase 1 avec laravel

--- connectin-api/vite.config.js
+++ connectin-api/vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/connectin/",
-  root: "resources", 
-  build: {
-    outDir: "../dist",
-    emptyOutDir: true,
-  },
+    plugins: [
+        laravel({
+            input: ["resources/css/app.css", "resources/js/app.jsx"],
+            refresh: true,
+        }),
+        tailwindcss(),
+        react(),
+    ],
+    server: {
+        host: "127.0.0.1",
+    },
 });
